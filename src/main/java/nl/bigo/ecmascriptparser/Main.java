c7bb248acb7355fc543e2806523ae30658410922
Bart Kiers <bkiers@gmail.com>
2014-04-13 22:08:27 +0200

    Initial commit.

---
+++ src/main/java/nl/bigo/ecmascriptparser/Main.java
@@ -0,0 +1,27 @@
+package nl.bigo.ecmascriptparser;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        ECMAScriptLexer lexer = new ECMAScriptLexer(new ANTLRInputStream("/((a)|(ab))((c)|(bc))/.exec(\"abc\")"));
+        ECMAScriptParser parser = new ECMAScriptParser(new CommonTokenStream(lexer));
+
+        ParseTree tree = parser.program();
+        System.out.println(tree.toStringTree(parser));
+
+//        while (true) {
+//            Token token = parser.anyToken().getStart();
+//            if (token.getType() == Token.EOF) {
+//                break;
+//            }
+//            System.out.printf("%-30s%s%n", ECMAScriptParser.tokenNames[token.getType()], token.getText());
+//        }
+    }
+}
